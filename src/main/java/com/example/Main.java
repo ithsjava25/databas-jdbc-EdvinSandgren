8bf37c8d3a7ced9f7dd37cd3689eebf495a55d38
Edvin Sandgren <229709012+EdvinSandgren@users.noreply.github.com>
2025-12-14 14:54:25 +0100

    Clears all tests locally

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,9 +1,8 @@
 package com.example;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
 
@@ -27,10 +26,155 @@ public class Main {
         }
 
         try (Connection connection = DriverManager.getConnection(jdbcUrl, dbUser, dbPass)) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter username:");
+            var user = scanner.nextLine();
+            System.out.println("Enter password:");
+            var password = scanner.nextLine();
+
+            boolean validCredentials = false;
+
+            String query = "select * from account";
+
+            try (PreparedStatement statement = connection.prepareStatement(query)) {
+                ResultSet result = statement.executeQuery();
+                while (result.next()) {
+                    if ((result.getString(2) + " " + result.getString(3))
+                            .equals(user + " " + password)) {
+                        validCredentials = true;
+                    }
+                }
+            }
+
+            if (!validCredentials) {
+                IO.println("Invalid username or password");
+            }
+
+            var option = scanner.nextLine();
+
+            switch (option) {
+                case "1" -> listMissions(connection);
+                case "2" -> listMissionById(connection, scanner);
+                case "3" -> listMissionsPerYear(connection, scanner);
+                case "4" -> createAccount(connection, scanner);
+                case "5" -> updatePassword(connection, scanner);
+                case "6" -> deleteAccount(connection, scanner);
+                default -> System.out.println("Invalid option");
+            }
+
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
         //Todo: Starting point for your code
+    }
+
+    private void deleteAccount(Connection connection, Scanner scanner) throws SQLException {
+
+        System.out.println("Enter userID:");
+        String userId = scanner.nextLine();
+
+        String delete = "delete from account where user_id = ?";
+
+        try (PreparedStatement statement = connection.prepareStatement(delete)) {
+            statement.setString(1, userId);
+            statement.executeUpdate();
+
+            System.out.println("Deleted account");
+        }
+    }
+
+    private void updatePassword(Connection connection, Scanner scanner) throws SQLException {
+
+        System.out.println("Enter userID:");
+        String userId = scanner.nextLine();
+
+        System.out.println("Enter new password:");
+        String password = scanner.nextLine();
+
+        String update = "update account set password = ? where user_id = ?";
+
+        try (PreparedStatement statement = connection.prepareStatement(update)) {
+            statement.setString(1, password);
+            statement.setString(2, userId);
+            statement.executeUpdate();
+
+            System.out.println("Updated password");
+        }
+
+    }
+
+    private void createAccount(Connection connection, Scanner scanner) throws SQLException {
+
+        System.out.println("Enter first name:");
+        String firstName = scanner.nextLine();
+
+        System.out.println("Enter last name:");
+        String lastName = scanner.nextLine();
+
+        System.out.println("Enter SSN:");
+        String ssn = scanner.nextLine();
+
+        System.out.println("Enter password:");
+        String password = scanner.nextLine();
+
+        String name = firstName.substring(0, 2) + lastName.substring(0, 2);
+
+        String insert = "insert into account (name, password, first_name, last_name, ssn) values (?, ?, ?, ?, ?)";
+
+        try (PreparedStatement statement = connection.prepareStatement(insert)) {
+            statement.setString(1, name);
+            statement.setString(2, password);
+            statement.setString(3, firstName);
+            statement.setString(4, lastName);
+            statement.setString(5, ssn);
+            statement.executeUpdate();
+
+            System.out.println("Account created successfully");
+        }
+    }
+
+    private void listMissionsPerYear(Connection connection, Scanner scanner) throws SQLException {
+
+        System.out.println("Enter year:");
+        int year = scanner.nextInt();
+
+        String query = "select count(launch_date) from moon_mission where year(launch_date) = ?";
+
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setInt(1, year);
+            ResultSet result = statement.executeQuery();
+            while (result.next()) {
+                System.out.println("Missions in " + year + ": " +  result.getLong(1));
+            }
+        }
+    }
+
+    private void listMissionById(Connection connection, Scanner scanner) throws SQLException {
+
+        System.out.println("Enter mission ID:");
+        var missionId = scanner.nextLine();
+
+        String query = "select * from moon_mission where mission_id = ?";
+
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setString(1, missionId);
+            ResultSet result = statement.executeQuery();
+            while (result.next()) {
+                System.out.println(result.getString(2));
+            }
+        }
+    }
+
+    private void listMissions(Connection connection) throws SQLException {
+
+        String query = "select * from moon_mission";
+
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            ResultSet result = statement.executeQuery();
+            while (result.next()) {
+                System.out.println(result.getString(2));
+            }
+        }
     }
 
     /**
